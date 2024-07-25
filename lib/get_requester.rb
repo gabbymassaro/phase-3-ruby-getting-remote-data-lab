1c425380a4c86d6bc3ffd97d2b08e14adedf1a1c
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-25 14:31:16 -0400

    Complete lab

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,25 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+require_relative '/Users/gabriella.romanowski/Development/code/phase-3/phase-3-ruby-getting-remote-data-lab/lib/get_requester.rb'
+
+class GetRequester
+
+  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    body = get_response_body
+    JSON.parse(body)
+  end
+
+end
